02f38fd37b4537cc1e49721ad8f28aa1787b4d53
konovalchuk <konoval_chuk@mail.ru>
2022-04-28 23:23:56 +0300

    fetch

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ let tabBoard = document.getElementById("tabBoard");
 let buttonComplain = document.getElementsByClassName("button-complain");
 let windowComplain = document.getElementById("complaint");
 
+//Кнопки окна Жалоба
+let buttonBack = document.getElementById("back");
+let buttonNext = document.getElementById("next");
+
+
 //Кнопки для переключения досок
 let buttonMountains = document.getElementById("mountains");
 let buttonAnimals = document.getElementById("animals");
@@ -16,23 +21,23 @@ let pinCity = document.getElementById("pin-city");
 
 //Открывает доску Горы
 buttonMountains.addEventListener("click", () => {
-    pinMountains.classList.remove("mountains");
-    pinAnimals.classList.add("animals");
-    pinCity.classList.add("city");
+    pinMountains.classList.remove("display-none");
+    pinAnimals.classList.add("display-none");
+    pinCity.classList.add("display-none");
 })
 
 //Открывает доску Животные
 buttonAnimals.addEventListener("click", () => {
-    pinAnimals.classList.remove("animals");
-    pinMountains.classList.add("mountains");
-    pinCity.classList.add("city");
+    pinAnimals.classList.remove("display-none");
+    pinMountains.classList.add("display-none");
+    pinCity.classList.add("display-none");
 })
 
 //Открывает доску Город
 buttonCity.addEventListener("click", () => {
-    pinCity.classList.remove("city");
-    pinAnimals.classList.add("animals");
-    pinMountains.classList.add("mountains");
+    pinCity.classList.remove("display-none");
+    pinAnimals.classList.add("display-none");
+    pinMountains.classList.add("display-none");
 })
 
 
@@ -49,3 +54,48 @@ buttonComplainArray.forEach(element => {
     })
 });
 
+/*
+const buttonNextArray = [...buttonNext];
+const buttonBackArray = [...buttonBack];
+
+buttonNextArray.forEach(element => {
+    element.addEventListener("click", () => {
+        windowComplain.classList.revove("tab-complaint-display-block");
+    })
+});
+
+buttonBackArray.forEach(element => {
+    element.addEventListener("click", () => {
+        windowComplain.classList.revove("tab-complaint-display-block");
+    })
+});
+
+buttonNext.addEventListener("click", () => {
+    windowComplain.classList.remove("tab-complaint-display-block");
+})
+
+buttonBack.addEventListener("click", () => {
+    windowComplain.classList.remove("tab-complaint-display-block");
+})
+*/
+
+fetch("https://625f0f2d873d6798e2b2501b.mockapi.io/pinterest/")   
+.then(response => {
+    return response.json();
+})
+.then(data => {
+data.forEach(item => {
+const foto = document.createElement("img");
+img.src = item.foto;
+
+const name = document.createElement("span");
+name.textContent = item.name;
+
+const container = document.createElement("div");
+
+container.appendChild(foto);
+container.appendChild(name);
+
+document.body.appendChild(container);
+}); 
+})
