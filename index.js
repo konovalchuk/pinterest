23a6a91c50c295955adcc684cfa6d491044dca16
konovalchuk <konoval_chuk@mail.ru>
2022-05-04 19:24:40 +0300

    ready

--- index.js
+++ index.js
@@ -1,51 +1,167 @@
-﻿let buttonBoard = document.getElementById("board");
+﻿﻿//Запрос на сервер для получения фото
+let windowComplain = document.getElementById("complaint");
+fetch("https://626afa0a6a86cd64adb6189e.mockapi.io/api/foto/foto")  
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    const picturesData = data.map(item => {
+const foto = document.createElement("img");
+foto.src = item.foto;
+foto.classList.add("pin");
+
+const name = document.createElement("span");
+name.textContent = item.name;
+
+const container = document.createElement("div");
+
+container.appendChild(foto);
+container.appendChild(name);
+return container;
+
+}); 
+
+const containers = [...document.getElementsByClassName("relative")];
+containers.forEach((item, i) => {
+    const spanDescr = document.createElement("span");
+    spanDescr.classList.add("descr");
+    item.appendChild(spanDescr);
+    const buttonSave = document.createElement("button");
+    buttonSave.classList.add("button-save");
+    buttonSave.innerHTML = "Сохранить";
+    spanDescr.appendChild(buttonSave);
+    const buttonComplaint = document.createElement("button");
+    buttonComplaint.classList.add("button-save");
+    buttonComplaint.classList.add("button-complain");
+    buttonComplaint.innerHTML = "Пожаловаться";
+    buttonComplaint.addEventListener("click", () => {
+        windowComplain.classList.add("display-block");
+    });
+    spanDescr.appendChild(buttonComplaint);
+    item.appendChild(picturesData[i]);
+})
+})
+
+//Кнопка и окно Жалобы
+let buttonComplain = document.getElementsByClassName("button-complain");
+
+// const buttonComplainArray = [...buttonComplain];
+// console.log('AAAAAAAAAAAAAa', buttonComplain);
+// buttonComplainArray.forEach(element => {
+//     console.log('element', element);
+//     element.addEventListener("click", () => {
+//         console.log('click');
+//         windowComplain.classList.add("display-block");
+//     })
+// });
+
+/*
+const buttons = document.getElementsByClassName('button-complain');
+for (const button of buttons) {
+  button.addEventListener('click', () => windowComplain.classList.add("display-block"));
+}*/
+
+// let span = [...document.getElementsByClassName('descr')];
+
+// span.forEach(element => {
+// element.addEventListener('click', event => {
+//     console.log('event', event.target.className);
+//       if (event.target.className === "button-complain") {
+//         windowComplain.classList.add("display-block");
+//     }
+
+//       })});
+/*let span = document.getElementsByClassName("descr");
+const spanArray = [...span];
+    spanArray.onclick = function(event) {
+        let button = event.target.closest('button');
+        windowComplain.classList.add("display-block");
+};
+    buttonComplain.onclick = function(event) {
+        let target = event.target;
+        windowComplain.classList.add("display-block");
+};
+buttonComplainArray.forEach(element => {
+    element.addEventListener("click", () => {
+        windowComplain.classList.add("display-block");
+    })
+});*/
+
+
+
+
+//Окно с выбором доски
+let buttonBoard = document.getElementById("board");
 let tabBoard = document.getElementById("tabBoard");
 
-let buttonComplain = document.getElementsByClassName("button-complain");
-let windowComplain = document.getElementById("complaint");
-
-//Кнопки для переключения досок
-let buttonMountains = document.getElementById("mountains");
-let buttonAnimals = document.getElementById("animals");
-let buttonCity = document.getElementById("city");
-
-//Доски
-let pinMountains = document.getElementById("pin-mountains");
-let pinAnimals = document.getElementById("pin-animals");
-let pinCity = document.getElementById("pin-city");
-
-//Открывает доску Горы
-buttonMountains.addEventListener("click", () => {
-    pinMountains.classList.remove("mountains");
-    pinAnimals.classList.add("animals");
-    pinCity.classList.add("city");
-})
-
-//Открывает доску Животные
-buttonAnimals.addEventListener("click", () => {
-    pinAnimals.classList.remove("animals");
-    pinMountains.classList.add("mountains");
-    pinCity.classList.add("city");
-})
-
-//Открывает доску Город
-buttonCity.addEventListener("click", () => {
-    pinCity.classList.remove("city");
-    pinAnimals.classList.add("animals");
-    pinMountains.classList.add("mountains");
-})
-
-
-//Открывает и закрывает окно с досками
+//Кнопка включения и выключения окна
 buttonBoard.addEventListener("click", () => {
     tabBoard.classList.toggle("tab-board-active");
 })
 
-const buttonComplainArray = [...buttonComplain];
+//Доски и кнопки для переключения досок
+let pinAnimals = document.getElementById("pin-animals");
+let pinNature = document.getElementById("pin-nature");
+let pinArt = document.getElementById("pin-art");
 
-buttonComplainArray.forEach(element => {
-    element.addEventListener("click", () => {
-        windowComplain.classList.add("tab-complaint-display-block");
-    })
-});
+let buttonAnimals = document.getElementById("animals");
+let buttonNature = document.getElementById("nature");
+let buttonArt = document.getElementById("art");
 
+function openAnimals(){
+    pinAnimals.classList.remove("display-none");
+    pinNature.classList.add("display-none");
+    pinArt.classList.add("display-none");
+}
+
+function openNature(){
+    pinNature.classList.remove("display-none");
+    pinAnimals.classList.add("display-none");
+    pinArt.classList.add("display-none");
+}
+
+function openArt(){
+    pinArt.classList.remove("display-none");
+    pinNature.classList.add("display-none");
+    pinAnimals.classList.add("display-none");
+}
+
+//Открывает доску Животные
+buttonAnimals.addEventListener("click", () => {
+    openAnimals();
+    localStorage.board = 1;
+})
+
+//Открывает доску Природа
+buttonNature.addEventListener("click", () => {
+    openNature();
+    localStorage.board = 2;
+})
+
+//Открывает доску Искусство
+buttonArt.addEventListener("click", () => {
+    openArt();
+    localStorage.board = 3;
+})
+
+//Кнопки выключения окна Жалобы
+let buttonBack = document.getElementById("back");
+let buttonNext = document.getElementById("next");
+
+buttonNext.addEventListener("click", () => {
+    windowComplain.classList.remove("display-block");
+})
+
+buttonBack.addEventListener("click", () => {
+    windowComplain.classList.remove("display-block");
+})
+
+//Проверяем ключ в localStorage, чтобы при обновлении нужная доска оставалась открытой
+if (localStorage.board == "2") {
+openNature();
+} else if (localStorage.board == "3"){
+openArt();
+} else {
+openAnimals();
+    localStorage.setItem("board", 1);
+}
